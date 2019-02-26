0543e46e79e40d141d4c59005ccfeff18fa91f5b
Justin Cole <jcole@alumni.colostate.edu>
2019-02-26 12:28:38 -0700

    added multiplication

--- Poly.cpp
+++ Poly.cpp
@@ -19,3 +19,102 @@ Poly Poly::operator - ()
     Poly result(-this->poly);
     return result;
 }
+
+Poly Poly::operator + (Poly p2)
+{
+    Poly result(poly);
+    for (int i = 0; i < poly.getDimension(); i++) {
+        result.setCoefficient(i,poly[i] + p2.getCoefficient(i));
+    }
+    return result;
+}
+
+Poly Poly::operator - (Poly p2)
+{
+    Poly result(poly);
+    for (int i = 0; i < poly.getDimension(); i++) {
+        result.setCoefficient(i,poly[i] - p2.getCoefficient(i));
+    }
+    return result;
+}
+
+Poly Poly::operator * (Poly p2)
+{
+    int n = this->getDegree();
+    if (n < p2.getDegree()) {
+        n = p2.getDegree();
+    }
+    Vector<Number> p0 = fill(*this,2*n);
+    Vector<Number> p1 = fill(p2,2*n);
+    p0 = fft(p0);
+    p1 = fft(p1);
+    Vector<Number> result = point_mult(p0,p1);
+    return Poly(fft(result));
+}
+
+Vector<Number> Poly::fill(Poly p, int size)
+{
+    Vector<Number> result(size);
+    for (int i = 0; i < p.getDegree(); i++) {
+        result[i] = p.getCoefficient(i);
+    }
+    return result;
+}
+
+Vector<Number> Poly::fft(Vector<Number> p)
+{
+    int n = p.getDimension();
+    if (n == 1) {
+        return p;
+    }
+    double arg = 8.0*atan(1.0)/((double) n);
+    Number omega_n(cos(arg),sin(arg));
+    Number omega(1.0,0.0);
+    Vector<Number> p0 = split('e',p);
+    Vector<Number> p1 = split('o',p);
+    Vector<Number> y0 = fft(p0);
+    Vector<Number> y1 = fft(p1);
+    Vector<Number> result(n);
+    for (int i = 0; i < n/2; i++) {
+        result[i] = y0[i] + omega*y1[i];
+        result[i + n/2] = y0[i] - omega*y1[i];
+        omega = omega*omega_n;
+    }
+    return result;
+}
+
+Vector<Number> Poly::split(char s, Vector<Number> p)
+{
+    int n = p.getDimension()/2 - 1;
+    Vector<Number> result(n);
+    int count = 0; 
+    for (int i = 0; i < p.getDimension(); i++) {
+        if (s == 'e' && i%2 == 0) {
+            result[count] = p[i];
+            count++;
+        }
+        else if (s == 'o' && i%2 != 0) {
+            result[count] = p[i];
+            count++;
+        }
+    }
+    return result;
+}
+
+Vector<Number> Poly::point_mult(Vector<Number> p1, Vector<Number> p2)
+{
+    Vector<Number> result(p1.getDimension());
+    for (int i = 0; i < p1.getDimension(); i++) {
+            result[i] = p1[i] * p2[i];
+    }
+    return result;
+}
+
+Poly operator * (Number n, Poly p)
+{
+    Poly result(p);
+    for (int i = 0; i < p.getDegree(); i++) {
+        result.setCoefficient(i,n * p.getCoefficient(i));
+    }
+    return result;
+}
--- Poly.h
+++ Poly.h
@@ -17,6 +17,7 @@ public:
     
     inline Number getCoefficient(int i) {return poly[i];}
     inline void setCoefficient(int i, Number num) {poly[i] = num;}
+    inline int getDegree() {return poly.getDimension();}
     
     // Overload operators
     Poly operator - ();
@@ -26,9 +27,15 @@ public:
     friend Poly operator * (Number n, Poly p);
     Poly operator / (Poly p2);
     
+    Vector<Number> fill(Poly p, int size);
+    Vector<Number> fft(Vector<Number> p);
+    Vector<Number> split(char s, Vector<Number> p);
+    Vector<Number> point_mult(Vector<Number> p1, Vector<Number> p2);
+    
     void print(char var);
     
 private:
     Vector<Number> poly;
 };
+
 #endif //POLY_H
